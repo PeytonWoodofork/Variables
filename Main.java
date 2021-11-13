b9c70a63ef1d13aad6084310f86ad13526c75200
PeytonWoodofork <peyton.woodfork@hgs.hiddengeniusproject.org>
2021-11-13 21:03:00 +0000

    added variable program

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+
+public class Main{
+    public static void main(String[]args){
+      Scanner sc = new Scanner(System.in);
+
+      String name = ("Peyton Woodfork");
+      String timeManagement = "set timer for certain activities";
+      String personalCommitment = "Get on top of all of my work";
+      String KPImetrics = "Number of interactions and transactions";
+
+      System.out.println("Hello my name is "+ name +", the time management strategy that I would like to be held accountable\n" + "to is" + timeManagement + ". I will personally commit to " + personalCommitment + ".\n " + "Mybusiness' mainKPI is/are " + KPImetrics + ",");
+    
+  }    
+   }
+
