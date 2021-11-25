0411edccd3e5e5c4dd78134d5db66b7a3ee0f891
PeytonWoodfork <Peyton.woodfork@hgs.hiddengeniusproject.org>
2021-11-25 02:19:36 +0000

    displaying variables

--- Main.java
+++ Main.java
@@ -11,7 +11,7 @@ public class Main{
       String personalCommitment = "Get on top of all of my work";
       String KPImetrics = "Number of interactions and transactions";
 
-      System.out.println("Hello my name is "+ name +", the time management strategy that I would like to be held accountable\n" + "to is" + timeManagement + ". I will personally commit to " + personalCommitment + ".\n " + "Mybusiness' mainKPI is/are " + KPImetrics + ",");
+      System.out.println("Hello my name is "+ name +", the time management strategy that I would like to be held accountable\n" + "to " + timeManagement + ". I will personally commit to " + personalCommitment + ".\n " + "My business' main KPI is/are " + KPImetrics + ",");
     
   }    
    }
